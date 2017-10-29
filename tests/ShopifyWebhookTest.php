2b6d27d7bfe78f04f017a77d68a853a0ca949623
Nicky Woolf <woolfnicky@gmail.com>
2017-10-29 13:23:26 -0700

    Use ShopifyWebhook To Verify Signed By Shopify

--- tests/ShopifyWebhookTest.php
+++ tests/ShopifyWebhookTest.php
@@ -1,13 +1,12 @@
 <?php
 
-use NickyWoolf\Shopify\Hmac;
+use NickyWoolf\Shopify\ShopifyWebhook;
 use PHPUnit\Framework\TestCase;
 
-class HmacTest extends TestCase
+class ShopifyWebhookTest extends TestCase
 {
-
     /** @test */
-    function check_valid_shopify_webhook()
+    function verify_webhook_signed_by_shopify()
     {
         $secret = 'SHOPIFY-CLIENT-SECRET';
         $body = json_encode([
@@ -18,25 +17,26 @@ class HmacTest extends TestCase
         ]);
         $header = base64_encode(hash_hmac('sha256', $body, $secret, true));
 
-        $hmac = new Hmac($secret);
+        $webhook = new ShopifyWebhook($secret);
 
-        $this->assertTrue($hmac->trustWebhook($header, $body));
+        $this->assertTrue($webhook->verify($header, $body));
     }
 
     /** @test */
-    function should_not_trust_webhook_signed_with_wrong_secret()
+    function can_not_verify_webhook_signed_by_shopify_using_different_secrets()
     {
         $secret = 'SHOPIFY-CLIENT-SECRET';
+        $differentSecret = 'DIFFERENT-CLIENT-SECRET';
         $body = json_encode([
             'timestamp' => 1234567890,
             'local' => 'en',
             'protocol' => 'https://',
             'shop' => 'example.myshopify.com',
         ]);
-        $header = base64_encode(hash_hmac('sha256', $body, 'A-WRONG-SECRET', true));
+        $header = base64_encode(hash_hmac('sha256', $body, $secret, true));
 
-        $hmac = new Hmac($secret);
+        $webhook = new ShopifyWebhook($differentSecret);
 
-        $this->assertFalse($hmac->trustWebhook($header, $body));
+        $this->assertFalse($webhook->verify($header, $body));
     }
 }
